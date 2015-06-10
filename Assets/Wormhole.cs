f5940777b646d1bb0239ea309531806940a8f6f6
Roberto <roberto.capiotto@gmail.com>
2015-06-10 16:04:43 +0200

    Wormhole working

--- Assets/Wormhole.cs
+++ Assets/Wormhole.cs
@@ -17,11 +17,10 @@ public class Wormhole : MonoBehaviour {
 	// I suppose that rocket loses all its characteristics of velocity and direction and follow the wormhole rules
 	// the wormhole shots right
 	void OnCollisionEnter (Collision collider) {
-		collider.transform.position=new Vector3 (exit.transform.position.x+this.transform.localScale.x/2+collider.transform.localScale.x/2+0.1f,
+		collider.transform.position=new Vector3 (exit.transform.position.x+this.transform.localScale.x/2+collider.transform.localScale.x/2+0.5f,
 		                                         exit.transform.position.y,
 		                                         exit.transform.position.z);
 		collider.rigidbody.velocity=new Vector3(0,0,0);
 		collider.rigidbody.AddForce(acceleration,0f,0f);
-		print("x: "+collider.rigidbody.velocity.x+" y: "+collider.rigidbody.velocity.y);
 	}
 }
