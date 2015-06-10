2bbfbf177c69de0967f86bdd41017f7ecea94a90
Roberto <roberto.capiotto@gmail.com>
2015-06-10 17:32:05 +0200

    Improved CameraMovement

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -5,14 +5,24 @@ public class CameraMovement : MonoBehaviour {
 
 	float camSize;
 	GameObject rocket;
+	Vector3 position;
 
+	// TODO: define how to move in the level
+	// we have to define boundaries?
+	// especially up and down
+	// eventually left
 	void Start () {
 		rocket = GameObject.Find ("Rocket");
 		camSize = Camera.mainCamera.orthographicSize;
+		position = new Vector3(0,0,0);
 	}
-	
-	// Update is called once per frame
+
+	// TODO: soft move of camera
+	// now is strongly switched
 	void Update () {
-		this.transform.position = new Vector3 (rocket.transform.position.x,rocket.transform.position.y,-10);
+		if(Mathf.Abs(rocket.transform.position.x-position.x)>camSize || Mathf.Abs(rocket.transform.position.y-position.y)>camSize){
+			position=new Vector3(rocket.transform.position.x,rocket.transform.position.y,-10);
+			this.transform.position = position;
+		}
 	}
 }
