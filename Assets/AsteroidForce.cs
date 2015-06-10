dc19c5aceb44cddd0c5ceb4ecd1e0669c90fb3a4
Roberto <roberto.capiotto@gmail.com>
2015-06-11 01:09:47 +0200

    Add asteroids and start implementing swing [not working]

---
+++ Assets/AsteroidForce.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidForce : MonoBehaviour {
+	
+	Vector3 myPos;
+	
+	void Start () {
+		// this instruction enable you having asteroid of different dimension
+		// i don't like to use it
+		//this.transform.localScale=new Vector3(Random.Range(0.6f,0.8f),Random.Range(0.4f,0.6f),1);
+
+		myPos = this.transform.position;
+		this.rigidbody.freezeRotation=true;
+		this.transform.rigidbody.constraints = RigidbodyConstraints.FreezeRotationX;
+		this.transform.rigidbody.constraints = RigidbodyConstraints.FreezeRotationY;
+		this.transform.rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+		this.rigidbody.freezeRotation=true;
+		
+	}
+	
+	void Update(){
+		this.transform.position = myPos;
+	}
+	
+	void OnCollisionStay(Collision collision)
+	{
+		//	TODO: set the correct force and define behaviour
+		collision.rigidbody.AddForce(new Vector3(Random.Range(-20,20),Random.Range(-20,20),0));
+	}
+}
---
+++ Assets/Swing.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swing : MonoBehaviour {
+
+	GameObject rocket;
+	GameObject joint;
+	bool collided=false;
+
+	void Start () {
+		rocket = GameObject.Find ("Rocket");
+	}
+
+	void Update () {
+		if(collided){
+			//Create HingeJoints
+			/*joint = gameObject.AddComponent<HingeJoint> ();
+			joint.axis = Vector3.back; /// (0,0,-1)
+			joint.anchor = Vector3.zero;
+			joint.connectedBody = anchor.rigidbody;
+			anchorJoint = anchor.AddComponent<HingeJoint> ();
+			anchorJoint.axis = Vector3.back; /// (0,0,-1)
+			anchorJoint.anchor = Vector3.zero;*/
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		collided=true;
+		// capisci da che parte arrivi e dove devi girare
+		// unisci gli oggetti
+		// quando sei arrivato spara
+	}
+
+	void OnCollisionExit(Collision collision){
+		collided = false;
+	}
+}
