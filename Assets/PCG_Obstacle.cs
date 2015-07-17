fc14557302bd571e7b7a80723576d3db595167e2
Roberto <roberto.capiotto@gmail.com>
2015-07-17 03:27:03 +0200

    Start obstacle generation (horizontal+vertical)
    NO ROUTINE. HARD CODE

--- Assets/PCG_Obstacle.cs
+++ Assets/PCG_Obstacle.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class PCG_Obstacle : MonoBehaviour {
 	
 	bool creation=false;
-	GameObject rocket,planet,cam;
+	GameObject rocket,planet,cam,asteroid;
 	Rocket rocketManager;
 	Planet planetManager;
 	Camera myCam;
-	float x,y,rand,camSize=5;
+	float x,y,rand,endRand,camSize=5;
 	Vector3 initialPosition;
 	Vector3 newPosition;
 	Vector3 endPosition;
@@ -20,6 +20,7 @@ public class PCG_Obstacle : MonoBehaviour {
 	public CameraMovement myCamera;
 	bool scrollCamera=false;
 	public int level=1;
+	public bool[] path;
 	
 	void Start () {
 		// get Camera
@@ -103,7 +104,7 @@ public class PCG_Obstacle : MonoBehaviour {
 		if(rand==0){
 			planet.transform.position=new Vector3(pos.x,pos.y-(level-1)*4-rand-camSize/2,0);
 		}
-		
+
 		int i=0,j=0;
 		for(;i<3;i++){
 			for(j=0;j<3;j++){
@@ -123,16 +124,175 @@ public class PCG_Obstacle : MonoBehaviour {
 		planetManager = planet.GetComponent ("Planet") as Planet;
 		planetManager.SetPlanetType("end");
 		// place endPlanet
-		rand=Random.Range(0,4);
-		print ("endRAND: "+rand);
+		endRand=Random.Range(0,4);
+		print ("endRAND: "+endRand);
 		if(rand==0)
 			planet.transform.position=new Vector3(pos.x+(level-1)*4*(i+1)+(i+1)*camSize*3/2,pos.y-(level-1)*4*(j+1)-camSize/2,0);
 		else
-			planet.transform.position=new Vector3(pos.x+(level-1)*4*(i+1)+(i+1)*camSize*3/2,pos.y-(level-1)*4*(j+1)-rand*camSize*3/2,0);
+			planet.transform.position=new Vector3(pos.x+(level-1)*4*(i+1)+(i+1)*camSize*3/2,pos.y-(level-1)*4*(j+1)-endRand*camSize*3/2,0);
 		
 		endPosition=planet.transform.position;
+		BuildPath();
 		level++;
 		return retPlan;
+	}
+
+	void BuildPath(){
+		path = new bool[9];
+		int i;
+		for(i=0;i<9;i++)
+			path[i]=false;
+
+		// first column
+		if(rand==0){
+			path[0]=true;
+		}
+		if(rand==1){
+			path[0]=true;
+			path[1]=true;
+		}
+		if(rand==2){
+			path[0]=true;
+			path[1]=true;
+			path[2]=true;
+		}
+		if(rand==3){
+			path[1]=true;
+			path[2]=true;
+		}
+		if(rand==4){
+			path[2]=true;
+		}
+
+		// last column
+		if(endRand==0){
+			path[6]=true;
+		}
+		if(endRand==1){
+			path[6]=true;
+			path[7]=true;
+		}
+		if(endRand==2){
+			path[6]=true;
+			path[7]=true;
+			path[8]=true;
+		}
+		if(endRand==3){
+			path[7]=true;
+			path[8]=true;
+		}
+		if(endRand==4){
+			path[8]=true;
+		}
+
+		// connect
+		if(path[0] && path[6])
+			path[3]=true;
+		if(path[0] && path[7])
+			path[3]=true;
+		if(path[1] && path[6])
+			path[4]=true;
+		if(path[1] && path[7])
+			path[4]=true;
+		if(path[1] && path[8])
+			path[5]=true;
+		if(path[2] && path[7])
+			path[4]=true;
+		if(path[2] && path[8])
+			path[5]=true;
+
+		// up couple
+		if(!(path[0] && path[1])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4+camSize*3/2;
+			y=startingCorner.y-(level-1)*4*(3/2.0f)-(3/2.0f)*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[3] && path[4])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*2+2*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*(3/2.0f)-(3/2.0f)*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[6] && path[7])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*3+3*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*(3/2.0f)-(3/2.0f)*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+
+		// down couple
+		if(!(path[1] && path[2])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4+camSize*3/2;
+			y=startingCorner.y-(level-1)*4*(5/2.0f)-(5/2.0f)*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[4] && path[5])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*2+2*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*(5/2.0f)-(5/2.0f)*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[7] && path[8])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*3+3*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*(5/2.0f)-(5/2.0f)*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+
+		// left couple
+		if(!(path[0] && path[3])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*(3/2.0f)+(3/2.0f)*camSize*3/2;
+			y=startingCorner.y-(level-1)*4-camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[1] && path[4])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*(3/2.0f)+(3/2.0f)*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*2-2*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[2] && path[5])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*(3/2.0f)+(3/2.0f)*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*3-3*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+
+		// right couple
+		if(!(path[3] && path[6])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*(5/2.0f)+(5/2.0f)*camSize*3/2;
+			y=startingCorner.y-(level-1)*4-camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[4] && path[7])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*(5/2.0f)+(5/2.0f)*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*2-2*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		if(!(path[5] && path[8])){
+			asteroid = Instantiate(Resources.Load("Asteroid")) as GameObject;
+			asteroid.name="Asteroid";
+			x=startingCorner.x+(level-1)*4*(5/2.0f)+(5/2.0f)*camSize*3/2;
+			y=startingCorner.y-(level-1)*4*3-3*camSize*3/2;
+			asteroid.transform.position=new Vector3(x,y,0);
+		}
+		
 	}
 	
 	public bool GetCreation(){
